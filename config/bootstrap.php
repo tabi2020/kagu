eab239c16610da28da9e6c62d598624d564c2078
eriko <eriko@eriko.local>
2016-08-11 13:29:40 +0900

    詳細ぺーじテンプレ

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -191,7 +191,7 @@ Request::addDetector('tablet', function ($request) {
  */
 
 Plugin::load('Migrations');
-
+Plugin::load('Sp');
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
